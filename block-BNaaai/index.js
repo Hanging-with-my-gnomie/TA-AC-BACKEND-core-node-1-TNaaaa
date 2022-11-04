02c5129904599cb632f3340691a3f0b49069bf07
Rajnish kumar <mr.rajnish2000@gmail.com>
2022-11-03 20:00:58 -0700

    block-BNaaai

---
+++ block-BNaaai/index.js
@@ -0,0 +1,13 @@
+//create file index.js
+var url = require("url");
+const parsedUrl = url.parse(
+  "https://blog.altcampus.io:80/students/register?name=altcampus&gender=male",
+  true
+);
+console.log(parsedUrl);
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.query);
+console.log (parsed);
+
+
+
